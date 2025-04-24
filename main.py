68eeb94e075974e2cc19d68e0552c82a8b856180
brian <brianarguello.vga>
2025-04-24 16:46:47 -0300

    a

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ directorio = pathlib.Path(input("Introduce la carpeta: "))
 
 print("Explorando carpeta....")
 
+lista = []
+
 for item in directorio.rglob("*"):
     if item.is_dir():
         print(f"Carpeta: {item}")
     elif item.is_file():
-        print(f"Archivo: {item}")
+        #print(f"Archivo: {item}")
+        lista.append(item)
+        print(f"esta en la lista{item}")
+
