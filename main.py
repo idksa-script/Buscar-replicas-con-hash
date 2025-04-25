67195a99a28aad481b198120aa0a0c2b3892c441
brian <brianarguello358@gmail.com>
2025-04-24 23:46:10 -0300

    agragando funcion hash

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import pathlib
+import hashlib as hs 
+
+def archivo_hash(archivo):
+    hash_obj = hs.new("sha256")
+    with open(archivo, "rb") as ar:
+        for bloque in  iter(lambda: ar.read(4096), b""):
+            hash_obj.update(bloque)
+    return hash_obj.hexdigest()
+
 
 #Pide la carpeta donde se buscara los archivos similares
 directorio = pathlib.Path(input("Introduce la carpeta: "))
 
 print("Explorando carpeta....")
 
-lista = []
+lista1 = []
+lista2 = []
 
 for item in directorio.rglob("*"):
     if item.is_dir():
         print(f"Carpeta: {item}")
     elif item.is_file():
         #print(f"Archivo: {item}")
-        lista.append(item)
-        print(f"esta en la lista{item}")
+        lista1.append(item)
+        lista2.append(archivo_hash(item))
+    
 
